4909565e9a2223c3b00b8cdfd0b8debe08ddeee7
httpdaniel <daniel.097@hotmail.com>
2019-12-05 23:58:09 +0000

    Update

--- src/main/java/com/daniel/Course.java
+++ src/main/java/com/daniel/Course.java
@@ -1,4 +1,61 @@
 package com.daniel;
 
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+
 public class Course {
+    private String name;
+    private ArrayList<Module> modules;
+    private ArrayList<Student> students;
+    private DateTime startDate;
+    private DateTime endDate;
+
+    public Course(String name, ArrayList<Module> modules, ArrayList<Student> students, DateTime startDate, DateTime endDate) {
+        this.name = name;
+        this. modules = modules;
+        this.students = students;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        this.modules = modules;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(DateTime endDate) {
+        this.endDate = endDate;
+    }
 }
---
+++ src/test/java/com/daniel/unitTest.java
@@ -0,0 +1,5 @@
+package com.daniel;
+
+public class unitTest {
+
+}
